15bba248a437140d6a691d841c55f53e40808b12
Boris Fomitchev <bfomitchev@nvidia.com>
2022-04-07 01:40:21 -0700

    Fixed Megatron export
    Signed-off-by: Boris Fomitchev <bfomitchev@nvidia.com>

--- Dockerfile
+++ Dockerfile
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ARG BASE_IMAGE=nvcr.io/nvidia/pytorch:22.03-py3
-
+#ARG BASE_IMAGE=nvcr.io/nvidia/pytorch:22.03-py3
+ARG BASE_IMAGE=gitlab-master.nvidia.com:5005/dl/dgx/pytorch:22.04_onnx-py3.4435575-devel-amd64
 
 # build an image that includes only the nemo dependencies, ensures that dependencies
 # are included first for optimal caching, and useful for building a development
--- nemo/collections/nlp/models/language_modeling/megatron_bert_model.py
+++ nemo/collections/nlp/models/language_modeling/megatron_bert_model.py
@@ -452,7 +452,7 @@ class MegatronBertModel(NLPModel):
         nvidia_torch_version = os.getenv('NVIDIA_PYTORCH_VERSION', None)
         if nvidia_torch_version is not None:
             NVIDIA_TORCH_MAJOR = int(nvidia_torch_version.split('.')[0])
-            NVIDIA_TORCH_MINOR = int(nvidia_torch_version.split('.')[1])
+            NVIDIA_TORCH_MINOR = 12 # int(nvidia_torch_version.split('.')[1])
 
             # Apex Persistent layer norm is supported from Nvidia PyTorch container v21.11
             if NVIDIA_TORCH_MAJOR < 21 or (NVIDIA_TORCH_MAJOR == 21 and NVIDIA_TORCH_MINOR < 11):
